69dad18cae983b7a4836ae63e01bc28d5a374ecc
woojung1234 <corall@naver.com>
2024-11-14 16:22:42 +0900

    Updates

--- netflix/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/movienet/'  // GitHub Pages에서 배포하는 경우
-      : '/'
-})
