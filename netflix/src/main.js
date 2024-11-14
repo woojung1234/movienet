6b3e502955fb06db47576b3d49f57be35757ea54
woojung1234 <corall5@naver.com>
2024-11-14 21:03:56 +0900

    as

--- netflix/src/main.js
+++ netflix/src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';  // 라우터 임포트
 
-createApp(App)
-    .use(router)  // 라우터 사용 설정
-    .mount('#app');
+createApp(App).mount('#app');
